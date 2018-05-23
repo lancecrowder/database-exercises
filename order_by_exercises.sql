6f0a09be970c65a9cd5758c430fe2da4594fd765
lancecrowder <thelancecrowder@gmail.com>
2018-05-23 15:11:37 -0500

    order_by_exercises.sql Completed steps 1&2 Create a new file called order_by_exercises.sql and copy in the contents of your where_exercises.sql. Modify your first query to order by first name. The first result should be Irena Reutenauer and the last result should be Vidya Simmen.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,7 +1,9 @@
 use employees;
 
 SELECT * FROM employees
-WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+      AND gender = 'M'
+ORDER BY first_name;
 
 SELECT * FROM employees
 WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
