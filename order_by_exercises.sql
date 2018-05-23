8a79b19675b82da37e999d5943342be972882b1f
lancecrowder <thelancecrowder@gmail.com>
2018-05-23 15:14:11 -0500

    Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -3,7 +3,7 @@ use employees;
 SELECT * FROM employees
 WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
       AND gender = 'M'
-ORDER BY first_name, last_name;
+ORDER BY last_name, first_name;
 
 SELECT * FROM employees
 WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
