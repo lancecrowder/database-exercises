d1179e9ef7f5b014acda4aed3c5eb88237622a1b
lancecrowder <thelancecrowder@gmail.com>
2018-05-23 16:17:29 -0500

    limit_exercises.sql completed

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,3 +4,14 @@ SELECT DISTINCT last_name
 FROM employees
   ORDER BY last_name DESC
 LIMIT 10;
+
+SELECT * FROM employees
+WHERE hire_date Like '199%' AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
+
+SELECT * FROM employees
+WHERE hire_date Like '199%' AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5 OFFSET 45;
+
