c093962cc1470af7a59192d14c58bad3dad49b9c
lancecrowder <thelancecrowder@gmail.com>
2018-05-24 11:25:48 -0500

    group_by_exercises.sql Completed

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -10,6 +10,12 @@ SELECT CONCAT(last_name,', ', first_name) FROM employees
 WHERE last_name LIKE 'E%E'
 GROUP BY last_name, first_name;
 
-SELECT last_name FROM employees
+SELECT last_name, COUNT(last_name)
+FROM employees
 WHERE last_name Like '%q%' AND last_name NOT LIKE '%qu%'
-GROUP BY last_name;
+GROUP BY last_name
+ORDER BY last_name;
+
+SELECT gender, COUNT(*) FROM employees
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+GROUP BY gender;
