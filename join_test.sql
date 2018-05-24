101497f09bd396031fe4dee7fbe1022ebb5f9fec
lancecrowder <thelancecrowder@gmail.com>
2018-05-24 16:32:41 -0500

    Joins Exercise began

---
+++ join_test.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE join_test_db;
+
+CREATE TABLE roles (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  role_id INT UNSIGNED DEFAULT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO roles (name) VALUES ('author');
+INSERT INTO roles (name) VALUES ('reviewer');
+INSERT INTO roles (name) VALUES ('commenter');
+
+INSERT INTO users (name, email, role_id) VALUES
+  ('bob', 'bob@example.com', 2),
+  ('joe', 'joe@example.com', 2),
+  ('sally', 'sally@example.com', 2),
+  ('adam', 'adam@example.com', NULL)
