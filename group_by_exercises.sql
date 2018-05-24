65f52bd5630af3a8652069d0d29de5846eb0351a
lancecrowder <thelancecrowder@gmail.com>
2018-05-24 10:56:14 -0500

    group_by_exercises.sql Completed Step 5

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -9,3 +9,7 @@ GROUP BY last_name;
 SELECT CONCAT(last_name,', ', first_name) FROM employees
 WHERE last_name LIKE 'E%E'
 GROUP BY last_name, first_name;
+
+SELECT last_name FROM employees
+WHERE last_name Like '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
