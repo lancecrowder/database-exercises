edfe8316b4b6e2ffa6d114a15f0caf465e85e9e4
lancecrowder <thelancecrowder@gmail.com>
2018-05-24 10:54:39 -0500

    group_by_exercises.sql Completed Step 4

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,6 +2,10 @@ USE employees;
 
 SELECT DISTINCT title FROM titles;
 
-SELECT last_name as 'Full Name' FROM employees
+SELECT last_name FROM employees
 WHERE last_name LIKE 'E%E'
 GROUP BY last_name;
+
+SELECT CONCAT(last_name,', ', first_name) FROM employees
+WHERE last_name LIKE 'E%E'
+GROUP BY last_name, first_name;
