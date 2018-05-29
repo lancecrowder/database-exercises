643a6bfedf3c09fbf6a530562a0aceabaf26c30e
lancecrowder <thelancecrowder@gmail.com>
2018-05-29 13:56:29 -0500

    subqueries_exercises.sql completed step 3

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -8,10 +8,31 @@ WHERE hire_date = (
   WHERE emp_no = 101010
 );
 
+SELECT title
+FROM titles
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM employees
+  WHERE first_name = 'Aamod'
+);
+
 SELECT DISTINCT title
 FROM titles
 WHERE emp_no IN (
   SELECT emp_no
   FROM employees
   WHERE first_name = 'Aamod'
+);
+
+SELECT CONCAT(first_name, ' ', last_name) as CurrentFemaleManagers
+FROM employees
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM dept_manager
+  WHERE emp_no IN (
+    SELECT emp_no
+    from employees
+    WHERE to_date > NOW()
+  )
+  AND gender = 'F'
 );
